44f25ee8d5280cd98fdb83af1abdf0a63a4d6874
John <jnk604@gmail.com>
2016-09-10 23:38:04 -0700

    added converter

--- js/app.js
+++ js/app.js
@@ -2,13 +2,19 @@ $( () => {
   var moment = require('moment');
   var rates = [];
   var show = 5;
+  var currentRate;
+
+  var $moreBtn = $("#rate-history-more");
+  var $form = $("#converter");
 
   (function getToday() {
     $.get({
       url: `http://api.fixer.io/latest?base=USD`,
       success(payload) {
-        rates.push({date: payload.date, rate: payload.rates["EUR"]});
+        currentRate = payload.rates["EUR"];
+        rates.push({date: payload.date, rate: currentRate});
         getHistory(payload.date);
+        // $("#converter input[type='text']").attr("placeholder", `1USD = ${currentRate}EUR`);
       }
     });
   })()
@@ -31,44 +37,58 @@ $( () => {
   function renderHistory() {
     for (var i = show - 5; i < show; i++) {
       var $boxLi = $("<li></li>");
-      $boxLi.addClass("box");
+      if (i === 0) {
+        $boxLi.addClass("box highlight");
+      } else {
+        $boxLi.addClass("box");
+      }
       var date = rates[i].date;
       $boxLi.text(date);
 
       var $infoUl = $("<ul></ul>");
 
-      var $rateLi = $("<li>" + rates[i].rate + "</li>");
+      var $rateLi = $("<li></li>");
       $rateLi.addClass("rate-conversion");
+      $rateLi.text(`1USD = ${rates[i].rate}EUR`);
 
       var $changeLi = $("<li></li>");
-      var diff = rates[i+1].rate - rates[i].rate;
+      var diff = rates[i].rate - rates[i+1].rate;
       $changeLi.addClass("rate-change");
       if (diff >= 0) {
         $changeLi.addClass("pos");
       } else {
         $changeLi.addClass("neg");
       }
-      $changeLi.text(diff.toFixed(4));
+      $changeLi.text(diff.toFixed(5));
 
       $infoUl.append($rateLi);
       $infoUl.append($changeLi);
       $boxLi.append($infoUl);
       $("ul#rate-history").append($boxLi);
 
-      $("#rate-history-more").prop("disabled",false);
+      $moreBtn.text("Show More");
+      $moreBtn.prop("disabled",false);
     }
   }
 
-  $("#rate-history-more").on("click", (e) => {
-    $("#rate-history-more").prop("disabled",true);
+  $form.on("submit", (e) => {
+    e.preventDefault();
+    var $historyLi = $("<li></li>");
+    var $inputEl = $("#converter input[type='text']");
+    var userInput = parseFloat($inputEl.val());
+    $inputEl.val("");
+
+    if (!isNaN(userInput)) {
+      var result = userInput * currentRate;
+      $historyLi.text(`${userInput.toFixed(2)} USD => ${result.toFixed(2)} EUR`);
+      $("#convert-history").prepend($historyLi);
+    }
+  });
+
+  $moreBtn.on("click", (e) => {
+    $moreBtn.prop("disabled",true);
+    $moreBtn.text("Loading");
     show += 5;
     getHistory(rates[rates.length-1].date);
   });
-
-  // <li class="box">
-  //   <ul>
-  //     <li class="rate-conversion"></li>
-  //     <li class="rate-change"></li>
-  //   </ul>
-  // </li>
 });
